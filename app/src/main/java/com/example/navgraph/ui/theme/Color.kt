7fb2aec3e5702e83a40f5e165c73e31529ef9337
wynprtm <wayanpratama38@gmail.com>
2025-01-27 15:39:19 +0700

    Configure Style

--- app/src/main/java/com/example/navgraph/ui/theme/Color.kt
+++ app/src/main/java/com/example/navgraph/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Navy200 = Color(0xFFC3DFED)
+val Navy500 = Color(0xFF2D3E50)
+val Navy700 = Color(0xFF152439)
+val Turquoise500 = Color(0xFF11C5C6)
--- app/src/main/java/com/example/navgraph/ui/theme/Theme.kt
+++ app/src/main/java/com/example/navgraph/ui/theme/Theme.kt
@@ -12,15 +12,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Navy200,
+    secondary = Turquoise500,
+    tertiary = Navy700,
+    secondaryContainer = Navy200,
+    onSecondaryContainer = Navy500,
+    onSurface = Navy200,
+    onSurfaceVariant = Navy200,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Navy500,
+    secondary = Turquoise500,
+    tertiary = Navy700,
+    secondaryContainer = Navy200,
+    onSecondaryContainer = Navy700,
+    onSurface = Navy700,
+    onSurfaceVariant = Navy500,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
