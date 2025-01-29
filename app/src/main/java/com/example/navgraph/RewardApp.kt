f34e49bb266d47f7e583e846b7fd22b484232dec
wynprtm <wayanpratama38@gmail.com>
2025-01-29 16:28:49 +0700

    Navigate Page with Bottom Bar

--- app/src/main/java/com/example/navgraph/RewardApp.kt
+++ app/src/main/java/com/example/navgraph/RewardApp.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
@@ -19,6 +20,7 @@ import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.navgraph.ui.navigation.NavigationItem
 import com.example.navgraph.ui.navigation.Screen
@@ -36,6 +38,10 @@ private fun BottomBar(
     NavigationBar(
         modifier = modifier
     ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+
         val navigationItems = listOf(
             NavigationItem(
                 title = stringResource(R.string.menu_home),
@@ -62,7 +68,7 @@ private fun BottomBar(
                     )
                 },
                 label = { Text(item.title) },
-                selected = false,
+                selected = currentRoute == item.screen.route,
                 onClick = {
                     navController.navigate(item.screen.route){
                         popUpTo(navController.graph.findStartDestination().id){
